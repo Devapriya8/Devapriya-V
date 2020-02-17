1a6e73e3e78d4ab1a761fd7b001a5e4c3191fde4
Devapriya8 <61120087+Devapriya8@users.noreply.github.com>
2020-02-17 14:32:09 +0530

    Create numberseries.py
    two inputs are give if a is lesser than b ,then a number series incremented and print the number series.if a is greater than b then the decremented number series is the output

---
+++ numberseries.py
@@ -0,0 +1,61 @@
+a=int(input("enter a"));
+b=int(input("enter b"));
+if(a<b):
+    for i in range(a,b,1):
+        print(i);
+else:
+    for i in range(a,b,-1):
+        print(i);
+        
+'''
+
+positive sample input
+4
+9
+
+positive sample output
+
+4
+5
+6
+7
+8
+
+negative sample input
+1
+1
+
+negative sample output
+
+
+'''a=int(input("enter a"));
+b=int(input("enter b"));
+if(a<b):
+    for i in range(a,b,1):
+        print(i);
+else:
+    for i in range(a,b,-1):
+        print(i);
+        
+'''
+
+positive sample input
+4
+9
+
+positive sample output
+
+4
+5
+6
+7
+8
+
+negative sample input
+1
+1
+
+negative sample output
+
+
+'''
