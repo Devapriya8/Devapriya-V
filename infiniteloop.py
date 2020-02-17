40d2e5452215d216b615e86cc7ba9ed3c845e3de
Devapriya8 <61120087+Devapriya8@users.noreply.github.com>
2020-02-17 13:53:10 +0530

    Create infiniteloop.py
    A while is given ,The loop executes till pressing 'q'.The loop exits after pressing 'q'.

---
+++ infiniteloop.py
@@ -0,0 +1,16 @@
+while True:
+    a=input()
+    if(a=='q'):
+        break;
+'''
+positive input sample
+q
+positive output sample
+loop exited
+
+negative input sample
+m
+negative output sample
+loop continues
+
+'''
