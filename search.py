79099acd74e425a0bb26a65ebb9da3549182ea0b
Devapriya8 <61120087+Devapriya8@users.noreply.github.com>
2020-02-17 14:44:13 +0530

    Create search.py
    to search the number in the list

---
+++ search.py
@@ -0,0 +1,16 @@
+a=[1,2,3,4,5,6,7,8,9];
+b=int(input());
+for i in range(len(a)):
+    if a[i]==b:
+        print("the element is found");
+''''
+
+positive sample input
+5
+positive sample output
+the element is found
+
+negative sample input12
+
+negative sample output
+''''
